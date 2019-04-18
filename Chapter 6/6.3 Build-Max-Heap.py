6135630586ac9356b3c422d07a016b1ebd53d700
Xuxiaoyu05 <34875319+Xuxiaoyu05@users.noreply.github.com>
2019-04-18 19:50:33 +0800

    Update 6.3 Build-Max-Heap.py

--- Chapter 6/6.3 Build-Max-Heap.py
+++ Chapter 6/6.3 Build-Max-Heap.py
@@ -6,9 +6,30 @@
 #          过程Build-Max-Heap对树中的非叶子结点都调用一次Max-Heapify
 #          采用“自底向上”的方法是因为在调用Max-Heapify过程时需要确保其左子树和右子树都是最大堆
 
+
+def Max_Heapify(A, root, heapsize):
+  left = 2 * root + 1    # 因为Python中下标从0开始，所以此处应该是2i+1
+  right = 2 * root + 2
+
+  larger = root
+
+  if left < heapsize and A[left] > A[larger]:  # 此处相较之前的Max_Heapify有所改动，因为在构建最大堆时，最后非叶子结点为heapsize//2-1，heapsize为len(A)所以不能等于
+    larger = left
+
+  if right < heapsize and A[right] > A[larger]:
+    larger = right
+
+  
+
+  if larger != root:
+    A[root], A[larger] = A[larger], A[root]
+    Max_Heapify(A, larger, heapsize)
+    
+  return A
+
 # 时间复杂度：O(n), 可在线性时间内，将一个无序数组构造成为一个最大堆
 def Build_Max_Heap(A):
-  heapsize = len(A) - 1
+  heapsize = len(A)
   for i in range(heapsize//2-1, -1, -1):    # 在Python中，由于下标是从0开始，所以heapsize//2-1是最后一个非叶子结点
     Max_Heapify(A, i, heapsize)
     
