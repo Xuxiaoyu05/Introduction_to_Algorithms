e7adf679a1c0b96f136bc836abc33edb2e8154d6
Xuxiaoyu05 <34875319+Xuxiaoyu05@users.noreply.github.com>
2019-04-27 15:44:56 +0800

    删除二叉搜索树中的结点

--- Chapter 12 二叉搜索树/12.3_II-1 BinaryTree_Delete.py
+++ Chapter 12 二叉搜索树/12.3_II-1 BinaryTree_Delete.py
@@ -23,8 +23,80 @@ class Tree:
     self.right = None
     self.parent = None
 
+def Minimum(root):
+  while root.left:
+    root = root.left
+  return root
 
-def Tree_Delete(root, z):
+# 定义子过程 Transplant：用另一棵子树替换一棵子树并成为其双亲的孩子结点。
+# 当 Transplant 用一棵以 v 为根的子树来替换一棵以 u 为根的子树时，结点 u 的双亲就变为结点 v 的双亲，且 v 成为 u 的双亲的相应孩子。
+
+def Transplant(root, u, v):
+  if u.parent == None: # 只有树根结点的父结点为 None
+    root = v
+  else:
+    if u == u.parent.left:
+      u.parent.left = v
+    else:
+      u.parent.right = v
   
+  if v != None:
+    v.parent = u.parent
+  
+def Tree_Delete(root, z):
+  if z.left == None:
+     Transplant(root, z, z.right)
+  elif z.right == None:
+    Transplant(root, z, z.left)
+  else:
+    y = Minimum(z.right)
+    if y.parent != z:
+      Transplant(root, y, y.right)
+      y.right = z.right
+      y.right.parent = y  # 此处是 y.right
     
+    Transplant(root, z, y)
+    y.left = z.left
+    y.left.parent = y
+  
+  return root
+
+
+def Create_BinaryTree(arr):
+  if len(arr) == 0:
+     return
+  
+  root = Tree(arr[0])
+  if len(arr) >= 2:
+    root.left = Create_BinaryTree(arr[1])
+    if root.left != None:
+      root.left.parent = root
+      
+  if len(arr) >= 3:
+    root.right = Create_BinaryTree(arr[2])
+    if root.right != None:
+      root.right.parent = root
+  
+  return root
+
+
+def PreOrder_Tree(root):
+  res = []
+  stack = []
+  stack.append(root)
+  
+  while stack:
+    cur = stack.pop()
+    res.append(cur.val)
     
+    if cur.right:
+      stack.append(cur.right)
+    if cur.left:
+      stack.append(cur.left)
+      
+  return res
+
+List = [12, [5, [2], [9]], [18, [15, [], [17]], [19]]]
+BinaryTree = Create_BinaryTree(List)
+z = BinaryTree.right
+print(PreOrder_Tree(Tree_Delete(BinaryTree, z)))
