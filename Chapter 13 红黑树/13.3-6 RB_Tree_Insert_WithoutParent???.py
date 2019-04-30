a1c8608778205a4a4aa39afc83cea24fa81fdadd
Xuxiaoyu05 <34875319+Xuxiaoyu05@users.noreply.github.com>
2019-04-30 12:59:24 +0800

    Create 13.3-6 RB_Tree_Insert_WithoutParent???.py

---
+++ Chapter 13 红黑树/13.3-6 RB_Tree_Insert_WithoutParent???.py
@@ -0,0 +1,15 @@
+# 习题 13.3-6：说明如果红黑树的表示中不提供父指针，应该如何有效地实现 RB_Insert。
+
+class RB_Tree_Node:
+  def __init__(self, val, color = "RED"):
+    self.val = val
+    self.left = None
+    self.right = None
+    self.color = color
+
+
+def Left_Rotate(root, x):
+  
+
+
+def Right_Rotate()
